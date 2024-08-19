726a0a7f1e028a18450efee6314dc2cab752c071
Nick Chernock <n.chernock@gmail.com>
2024-08-18 21:15:34 -0400

    implemented defined printAddressBook / returnToMainMenu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 
 void mainMenu(AddressBook myAddressBook);
+void printAddressBook(AddressBook myAddressBook);
+void returnToMainMenu(AddressBook myAddressBook);
 
 int main() {
     
@@ -29,36 +31,57 @@ void mainMenu(AddressBook myAddressBook) {
     cout << "\t5. Clear Address Book" << endl;
     cout << "\t6. Quit" << endl;
     cout << "----------------------------------------------" << endl << endl;
+    cout << "Enter a selection: ";
 
-
-    while (cin >> userSelection)
+    while (!(cin >> userSelection))
     {
-        switch (userSelection)
+        cout << "Invalid Input, Please enter a valid selection...\n";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+
+    switch (userSelection)
         {
         case 1:
-            cout << "User Selection: " <<  userSelection << endl;
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
             break;
         case 2:
-            cout << "User Selection: " <<  userSelection << endl;
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
             break;
         case 3:
-            cout << "User Selection: " <<  userSelection << endl;
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
             break;
         case 4:
-            cout << "User Selection: " <<  userSelection << endl;
-            myAddressBook.printBook();
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
+            printAddressBook(myAddressBook);
             break;
         case 5:
-            cout << "User Selection: " <<  userSelection << endl;
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
             break;
         case 6:
-            cout << "User Selection: " <<  userSelection << endl;
+            cout << "[DEBUG] User Selection: " <<  userSelection << endl;
             break;
         default:
             break;
-        }
     }
-    
+}
 
+void printAddressBook(AddressBook myAddressBook) {
+    cout << endl;
+    myAddressBook.printBook();
+    cout << endl;
+    returnToMainMenu(myAddressBook);
+}
 
+void returnToMainMenu(AddressBook myAddressBook) {
+    string enterInput;
+
+    cout << "Hit Enter to return to the Main Menu....";
+    cin >> enterInput;
+    while (enterInput != "\n") {
+        cout << "Please hit enter to continue.....\n";
+        cin.clear();
+     cin.ignore();
+    }
+    mainMenu(myAddressBook);
 }
